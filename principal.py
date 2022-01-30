598c5d0192683172e265ceef5f6b224cc411e440
Gilmar <gilmar.jesus@gmail.com>
2022-01-30 16:37:11 -0300

    Leitura de arquivo com parametro readlines.

--- principal.py
+++ principal.py
@@ -1,4 +1,8 @@
 arquivo_contato = open('dados/contatos.csv', encoding='latin_1')
 
-for linha in arquivo_contato:
-    print(linha, end='')
+conteudo = arquivo_contato.readlines()
+
+print(conteudo, end= '')
+
+# for linha in arquivo_contato:
+#     print(linha, end='')
