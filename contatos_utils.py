0dd5017f763fc03fb98a8e6c8209199099ae41cc
Gilmar <gilmar.jesus@gmail.com>
2022-01-31 17:41:58 -0300

    Gravando contatos no formato json.

--- contatos_utils.py
+++ contatos_utils.py
@@ -1,4 +1,4 @@
-import csv, pickle
+import csv, pickle, json
 from contato import Contato
 
 
@@ -21,7 +21,29 @@ def contatos_para_pickle(contatos, caminho):
     with open(caminho, mode='wb') as arquivo:
         pickle.dump(contatos, arquivo)
 
+
 def pickle_para_contatos(caminho):
     with open(caminho, mode='rb') as arquivo:
         contatos = pickle.load(arquivo)
+    return contatos
+
+
+def contatos_para_json(contatos, caminho):
+    with open(caminho, mode='w') as arquivo:
+        json.dump(contatos, arquivo, default=_contato_para_json)
+
+
+def _contato_para_json(contato):
+    return contato.__dict__
+
+
+def json_para_contatos(caminho):
+    contatos = []
+
+    with open(caminho) as arquivo:
+        contatos_json = json.load(arquivo)
+
+        for contato in contatos_json:
+            c = Contato(**contato)
+            contatos.append(c)
     return contatos
