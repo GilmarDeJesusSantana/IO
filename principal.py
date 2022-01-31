f21777bfaabfb222d1b8c8dc540e86491b12dc58
Gilmar <gilmar.jesus@gmail.com>
2022-01-31 17:22:27 -0300

    Lendo arquivo arquivo pickle.

--- principal.py
+++ principal.py
@@ -1,8 +1,9 @@
 import contatos_utils
 
 try:
-    contatos = contatos_utils.csv_para_contatos('dados/contatos.csv')
-    contatos_utils.contatos_para_pickle(contatos, 'dados/contatos.pickle')
+    # contatos = contatos_utils.csv_para_contatos('dados/contatos.csv')
+    # contatos_utils.contatos_para_pickle(contatos, 'dados/contatos.pickle')
+    contatos = contatos_utils.pickle_para_contatos('dados/contatos.pickle')
     for contato in contatos:
         print(f'{contato.id} - {contato.nome} - {contato.email}')
 except FileNotFoundError:
