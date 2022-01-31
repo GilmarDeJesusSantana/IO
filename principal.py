698547a717d82d5e97267c80796c41abc4dca4d3
Gilmar <gilmar.jesus@gmail.com>
2022-01-31 16:19:58 -0300

    Usado with para abrir o arquivo.

--- principal.py
+++ principal.py
@@ -1,13 +1,8 @@
 try:
-    arquivo_contato = open('dados/contatos.csv', encoding='latin_1')
-
-    conteudo = arquivo_contato.readlines()
-
-    for linha in arquivo_contato:
-        print(linha, end='')
+    with open('dados/contatos.csv', encoding='latin_1') as arquivo_contato:
+        for linha in arquivo_contato:
+            print(linha, end='')
 except FileNotFoundError:
     print('Arquivo não encontrado.')
 except PermissionError:
     print('Sem permissão de escrita.')
-finally:
-    arquivo_contato.close()
